3c351bb919548971387466b8b72aeb33374bb53a
dhruvv <dhruva.venkataraman@gmail.com>
2020-09-17 22:41:38 +0530

    Update bot.py
    Added livescore feature

--- bot.py
+++ bot.py
@@ -57,6 +57,10 @@ async def on_table_command(ctx):
     table = str(getIplTable())
     await ctx.send("```"+table+"```") 
 
+@bot.commend(name='score', help='Returns the score of the current match. Data scraped from IPLT20.com')
+async def on_score_command(ctx):
+    await ctx.send('Score command invoked')
+
 bot.run(token)
 
 

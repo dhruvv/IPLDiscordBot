78a764448884a99bb504733c7bc1be8a68506408
dhruvv <dhruva.venkataraman@gmail.com>
2020-09-17 16:01:03 +0530

    Table and Command
    Get IPL Table from official IPL website via BeautifulSoup 4 and respond to events from Discord API using discord.py. Bot now recognizes the "%table" command!

--- bot.py
+++ bot.py
@@ -1,11 +1,51 @@
 from bs4 import BeautifulSoup
 import urllib.request as urllib
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+global token
+token = os.getenv('DISCORD_TOKEN')
+bot = commands.Bot(command_prefix="%")
+
+    
 ipltablepage = "https://www.iplt20.com/points-table/2020"
-ipltableData = urllib.urlopen(ipltablepage)
-soup = BeautifulSoup(ipltableData, 'html.parser')
-iplTable = soup.find('table', attrs={'class':'standings-table'})
-#print(iplTable.prettify())
-iplTableByPos = iplTable.find_all('tr')
-print(iplTableByPos[1].find_all('td')[0].contents)
+
+
+def getIplTable():
+    ipltableData = urllib.urlopen(ipltablepage)
+    soup = BeautifulSoup(ipltableData, 'html.parser')
+    iplTable = soup.find('table', attrs={'class':'standings-table'})
+    #print(iplTable.prettify())
+    iplTableByPos = iplTable.find_all('tr')
+    iplTableByPos.pop(0)
+
+    attrList = []
+    for pos in iplTableByPos:
+        tdList = pos.find_all('td')
+        posList = []
+        for td in tdList:
+            c = td.contents
+            if len(c) > 4:
+                tName = c[3].find('span',attrs={'class':'standings-table__team-name--short'}).contents
+                posList.append(tName)
+            else:
+                posList.append(c)
+        posList.pop(-1)
+        attrList.append(posList)
+    return attrList
+
+
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord! ')
+
+@bot.command(name='table')
+async def on_table_command(ctx):
+    await ctx.send("Table command invoked")
+
+bot.run(token)
 
 

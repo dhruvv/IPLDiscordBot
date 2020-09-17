db4233ff0a4bfa8ec96ccc216789faccd3d10ad4
dhruvv <dhruva.venkataraman@gmail.com>
2020-09-17 23:56:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -60,6 +60,9 @@ async def on_table_command(ctx):
 @bot.command(name='score', help='Returns the score of the current match. Data scraped from IPLT20.com')
 async def on_score_command(ctx):
     await ctx.send('Score command invoked')
+@bot.command(name='nextmatch', help='Returns the next match of PARAM. Usage: %nextmatch TEAMNAME')
+async def on_nextmatch_command(ctx, teamname):
+    await ctx.send('Next match command invoked with' + teamname + 'as param')
 
 bot.run(token)
 
